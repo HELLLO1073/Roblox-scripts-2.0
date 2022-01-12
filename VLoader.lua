3233e168257dfe877f3aa6b54aa55a778c31715f
H3 <67812277+HELLLO1073@users.noreply.github.com>
2022-01-12 12:39:38 +1100

    Create VLoader.lua

---
+++ VLoader.lua
@@ -0,0 +1,12 @@
+if game.PlaceId == 4581966615 then -- anomic 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/Anomic/A2.lua", true))()
+    else if game.PlaceId == 4738545896 then -- shooutout
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/ShootOut/OS.lua", true))()
+        else if game.PlaceId == 746820961 then -- unit 1968
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/Unit1968/main(source).lua", true))()     
+            else if game.PlaceId == 413424176 then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/CloneTycoon/CT-Destroyer.lua", true))()   
+            end
+        end
+    end   
+end

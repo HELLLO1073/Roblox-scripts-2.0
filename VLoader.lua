c3083d6c9e67fb5f35e85c77d1b171243e7b167a
H3 <67812277+HELLLO1073@users.noreply.github.com>
2022-01-20 17:01:46 +1100

    Update VLoader.lua

--- VLoader.lua
+++ VLoader.lua
@@ -4,9 +4,9 @@ if game.PlaceId == 4581966615 then -- anomic
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/ShootOut/OS.lua", true))()
         else if game.PlaceId == 746820961 then -- unit 1968
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/Unit1968/main(source).lua", true))()     
-            else if game.PlaceId == 413424176 then
+            else if game.PlaceId == 413424176 then -- clone tycoon
             loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/CloneTycoon/CT-Destroyer.lua", true))()   
-                else if game.PlaceId == 6717202874 then
+                else if game.PlaceId == 6717202874 then -- easy fps
                     loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Roblox-scripts-2.0/main/Games/EasyFPS/EZFPS(source).lua", true))()   
                 end
             end

c2ec67576fca1d2e34055c3d9193758a3c3aba24
Bonfire <79977742+BonfireDevelopment@users.noreply.github.com>
2022-07-22 22:49:24 -0700

    Update venyxUI-mod.lua

--- UIS/venyxUI-mod.lua
+++ UIS/venyxUI-mod.lua
@@ -198,6 +198,8 @@ do
 	
 end
 
+local DevList = {"BonfireHubWhen", "US3RNAME_3ACC", "Tempo_Tantrum"}
+
 local function ApplyDev(v)
     local s,e = pcall(function()
         v.Head.PlayerDisplay.Wanted.Text = "Exploit Dev"
